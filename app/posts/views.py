90d86fab3631698528c791a330d05f2ff245fdfb
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-03 21:57:30 -0700

    Post_list 작성

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from posts.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(author__to_user_relation__from_user=request.user,
+                                author__to_user_relation__relation_type='f')
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'posts/post_list.html', context)
