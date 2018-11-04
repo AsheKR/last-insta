afdb1995a2f781f5cc9b4e484773324db01018e8
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-03 23:20:34 -0700

    post_list에 자신의 post도 나오게 수정

--- app/posts/views.py
+++ app/posts/views.py
@@ -8,7 +8,7 @@ from posts.models import Post
 @login_required
 def post_list(request):
     posts = Post.objects.filter(author__to_user_relation__from_user=request.user,
-                                author__to_user_relation__relation_type='f')
+                                author__to_user_relation__relation_type='f') | Post.objects.filter(author=request.user)
     context = {
         'posts': posts
     }
